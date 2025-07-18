0cbc2f5e7bfff220d9719a0808c888c00e5ce0ad
Mario Munera <samuelmariomunera@gmail.com>
2025-07-17 20:51:08 -0500

    2

---
+++ src/ejercicio1.js
@@ -0,0 +1,10 @@
+let numero = 7;
+
+// Función para verificar si un número es par o impar
+function verificarParImpar(num) {
+    if (num % 2 === 0) {
+        return "El número es par";
+    } else {
+        return "El número es impar";
+    }
+}
